f4a1d96a0053c5faadd7cbdbd68cec1d5d1817a9
Toby Batch <tobias@neontribe.co.uk>
2019-01-16 10:15:59 +0000

    Fixed sql extensuion;

--- prod/startup.sh
+++ prod/startup.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 # If we are mysql wait for it.
-if [ "${DATABASE_URL}" == mysql* ]; then
-    until mysql -u lamp -plamp -h db lamp -e "show tables"; do
+if [[ "${DATABASE_URL}" == mysql* ]]; then
+    echo Using Mysql DB
+    until mysql -u kimai -pkimai -h db kimai -e "show tables"; do
         >&2 echo "Mysql is unavailable - sleeping"
         sleep 5
     done
+else
+    echo Using non mysql DB
 fi
 
 cat <<EOF > /opt/kimai/.env
@@ -16,6 +19,7 @@ APP_SECRET=${APP_SECRET}
 DATABASE_URL=${DATABASE_URL}
 MAILER_URL=${MAILER_URL}
 EOF
+echo "# Check /etc/apache2/sites-available/000-default.conf for env vars" > /opt/kimai/.env
 if [ "${TRUSTED_PROXIES}" != 'false' ]; then
     echo TRUSTED_PROXIES=${TRUSTED_PROXIES} >> /opt/kimai/.env
 fi
@@ -25,7 +29,8 @@ fi
 
 # If the schema does not exist then create it (and run the migrations)
 TABLE_COUNT=$(/opt/kimai/bin/console doctrine:query:dql "Select u from App\Entity\User u")
-if [ "$?" != 0 ]; then
+STATUS=$?
+if [ -z "$TABLE_COUNT" ]; then
     # We can't find the users table.  We'll usae this to guess we don't have a schema installed.
     # Is there a better way of doing this?
     /opt/kimai/bin/console -n doctrine:schema:create
@@ -53,7 +58,8 @@ for initfile in /var/tmp/init-sql/*; do
 done
 
 # Warm up the cache
+/opt/kimai/bin/console cache:clear --env=prod
 /opt/kimai/bin/console cache:warmup --env=prod
 
 # Start listening
-apache2ctl -D FOREGROUND
+/usr/sbin/apache2ctl -D FOREGROUND
