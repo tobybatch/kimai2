d5c0a73424890b631e574c5140cdbbfb601ee3e5
Toby Batch <tobias@neontribe.co.uk>
2019-12-09 16:00:04 +0000

    Added a push shell script

---
+++ bin/push.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker push kimai/kimai2:apache-debian-1.5-dev
+docker push kimai/kimai2:fpm-alpine-1.5-dev
+docker push kimai/kimai2:apache-debian-1.5-prod
+docker push kimai/kimai2:fpm-alpine-1.5-prod
+docker push kimai/kimai2:apache-debian-master-dev
+docker push kimai/kimai2:fpm-alpine-master-dev
+docker push kimai/kimai2:apache-debian-master-prod
+docker push kimai/kimai2:fpm-alpine-master-prod
