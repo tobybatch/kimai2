1f67bf5bad2ea6327ea33bf1aade68b42379c62f
Thomas Strohmeier <tstrohmeier@users.noreply.github.com>
2023-03-21 23:10:42 +0100

    Fix startup.sh

--- assets/startup.sh
+++ assets/startup.sh
@@ -9,6 +9,29 @@ function config() {
   if [ -z "$memory_limit" ]; then
     memory_limit=256
   fi
+  
+  # Parse sql connection data
+  if [ ! -z "$DATABASE_URL" ]; then
+    DB_TYPE=$(awk -F '[/:@]' '{print $1}' <<< "$DATABASE_URL")
+    DB_USER=$(awk -F '[/:@]' '{print $4}' <<< "$DATABASE_URL")
+    DB_PASS=$(awk -F '[/:@]' '{print $5}' <<< "$DATABASE_URL")
+    DB_HOST=$(awk -F '[/:@]' '{print $6}' <<< "$DATABASE_URL")
+    DB_PORT=$(awk -F '[/:@]' '{print $7}' <<< "$DATABASE_URL")
+    DB_BASE=$(awk -F '[/?]' '{print $4}' <<< "$DATABASE_URL")
+  else
+    DB_TYPE=${DB_TYPE:mysql}
+    if [ "$DB_TYPE" == "mysql" ]; then
+      export DATABASE_URL="${DB_TYPE}://${DB_USER:=kimai}:${DB_PASS:=kimai}@${DB_HOST:=sqldb}:${DB_PORT:=3306}/${DB_BASE:=kimai}"
+    else
+      echo "Unknown database type, cannot proceed. Only 'mysql' is supported, received: [$DB_TYPE]"
+      exit 1
+    fi
+  fi
+
+  re='^[0-9]+$'
+  if ! [[ $DB_PORT =~ $re ]] ; then
+     DB_PORT=3306
+  fi
 
   echo "Wait for MySQL DB connection ..."
   until php /dbtest.php $DB_HOST $DB_BASE $DB_PORT $DB_USER $DB_PASS; do
